da7c75b17cad211ca2fc947bd3d7b1c23b805910
Jerridan Quiring <jerridan@gmail.com>
2020-08-12 20:08:52 -0400

    Add tests for quality at 0, and quality deduction when sell by date has passed

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,14 +1,14 @@
 const { Shop, Item } = require("../src/gilded_rose");
 
 describe("Gilded Rose", function () {
-  it("gives all items a 'sellIn' and a 'quantity' value", () => {
+  it("gives all items a 'sellIn' and a 'quality' value", () => {
     const sellIn = 10;
-    const quantity = 20;
+    const quality = 20;
 
-    const item = new Item("foo", sellIn, quantity);
+    const item = new Item("foo", sellIn, quality);
 
     expect(item.sellIn).toEqual(sellIn);
-    expect(item.quality).toEqual(quantity);
+    expect(item.quality).toEqual(quality);
   });
 
   it("lowers the value of 'sellIn' by 1 each day for each item", () => {
@@ -16,10 +16,10 @@ describe("Gilded Rose", function () {
     const item2 = new Item("item2", 5, 0);
     const gildedRose = new Shop([item1, item2]);
 
-    const items = gildedRose.updateQuality();
+    gildedRose.updateQuality();
 
-    expect(items[0].sellIn).toEqual(9);
-    expect(items[1].sellIn).toEqual(4);
+    expect(item1.sellIn).toEqual(9);
+    expect(item2.sellIn).toEqual(4);
   });
 
   it("lowers the value of 'quality' by 1 each day for each item", () => {
@@ -29,9 +29,33 @@ describe("Gilded Rose", function () {
     const item2 = new Item("item2", sellIn, 5);
     const gildedRose = new Shop([item1, item2]);
 
-    const items = gildedRose.updateQuality();
+    gildedRose.updateQuality();
 
-    expect(items[0].quality).toEqual(9);
-    expect(items[1].quality).toEqual(4);
+    expect(item1.quality).toEqual(9);
+    expect(item2.quality).toEqual(4);
+  });
+
+  it("lowers the quality of an item by 2 each day if the sell by date has passed", () => {
+    const sellIn = 0;
+    const quality = 4;
+
+    const item = new Item("item", sellIn, quality);
+    const gildedRose = new Shop([item]);
+
+    gildedRose.updateQuality();
+
+    expect(item.quality).toEqual(2);
+  });
+
+  it("does not allow the quality of an item to become negative", () => {
+    const sellIn = 0;
+    const quality = 0;
+
+    const item = new Item("item", sellIn, quality);
+    const gildedRose = new Shop([item]);
+
+    gildedRose.updateQuality();
+
+    expect(item.quality).toEqual(0);
   });
 });
